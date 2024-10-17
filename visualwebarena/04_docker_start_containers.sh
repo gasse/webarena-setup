5700029d0dd6cee1c0e92c6997ac4e0d3932d37c
Maxime Gasse <maxime.gasse@gmail.com>
2024-10-17 14:11:37 -0400

    Public hostname redirect hack

--- visualwebarena/04_docker_start_containers.sh
+++ visualwebarena/04_docker_start_containers.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+source 00_vars.sh
+
+# 1. make sure requests to HOSTNAME are made to the local ip (avoid firewall issues)
+
+HOSTNAME_REDIRECT="127.0.0.1 ${PUBLIC_HOSTNAME}"
+# Check if the redirect hack already exists in /etc/hosts
+if ! grep -Fxq "$HOSTNAME_REDIRECT" /etc/hosts; then
+    # If the redirect hack doesn't exist, append it to /etc/hosts
+    echo "Applying redirect hack to /etc/hosts"
+    echo "${HOSTNAME_REDIRECT}" | tee -a /etc/hosts
+else
+    echo "Redirect already in /etc/hosts, skipping"
+    echo "${HOSTNAME_REDIRECT}"
+fi
+
+# 2. start the containers
+
 docker start shopping
 docker start forum
 docker start wikipedia
