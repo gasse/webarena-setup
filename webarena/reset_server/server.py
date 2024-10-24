eb0bf0c05e8dc8fad9f5ae4b04f7a8da9d5b3bc4
Maxime Gasse <maxime.gasse@gmail.com>
2024-10-24 14:01:49 -0400

    better logging

--- webarena/reset_server/server.py
+++ webarena/reset_server/server.py
@@ -1,32 +1,40 @@
+import argparse
 import http.server
+import logging
 import socketserver
 import subprocess
-import argparse
+import sys
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"))
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         # Extract path
         parsed_path = self.path
-        print(f"Request received {parsed_path}")
+        logger.info(f"Request received {parsed_path}")
         if parsed_path == '/reset':
             print("Running reset script...")
             try:
                 # Execute the reset script
                 subprocess.run(['bash', 'reset.sh'], check=True)
-                print("Reset successful!")
+                logger.info("Reset successful!")
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 self.wfile.write(b'Reset script executed successfully')
             except subprocess.CalledProcessError as e:
-                print("Reset failed :(")
+                logger.info("Reset failed :(")
                 self.send_response(500)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 self.wfile.write(f'Error executing reset script: {e}'.encode())
         else:
-            print("Wrong request")
+            logger.info("Wrong request")
             self.send_response(404)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
@@ -40,6 +48,5 @@ args = parser.parse_args()
 
 # Run the server
 with socketserver.TCPServer(('', args.port), CustomHandler) as httpd:
-    print(f'Serving on port {args.port}...')
+    logger.info(f'Serving on port {args.port}...')
     httpd.serve_forever()
-
