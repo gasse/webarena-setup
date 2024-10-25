0b563fd48c7c8be9616d4f080da28dd6060b3077
Maxime Gasse <maxime.gasse@gmail.com>
2024-10-25 15:52:16 -0400

    fix

--- webarena/01_docker_load_images.sh
+++ webarena/01_docker_load_images.sh
@@ -3,6 +3,8 @@
 # stop if any error occur
 set -e
 
+source 00_vars.sh
+
 docker load --input ${ARCHIVES_LOCATION}/shopping_final_0712.tar
 docker load --input ${ARCHIVES_LOCATION}/shopping_admin_final_0719.tar
 docker load --input ${ARCHIVES_LOCATION}/postmill-populated-exposed-withimg.tar
