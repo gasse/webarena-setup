c1b9a7e161b9e427edc425c6691eae27c79d1ed0
Maxime Gasse <maxime.gasse@gmail.com>
2024-11-08 15:32:21 -0500

    make reddit server more responsive

--- visualwebarena/05_docker_patch_containers.sh
+++ visualwebarena/05_docker_patch_containers.sh
@@ -5,8 +5,19 @@ set -e
 
 source 00_vars.sh
 
+# reddit - make server more responsive
+docker exec forum sed -i \
+  -e 's/^pm.max_children = .*/pm.max_children = 32/' \
+  -e 's/^pm.start_servers = .*/pm.start_servers = 10/' \
+  -e 's/^pm.min_spare_servers = .*/pm.min_spare_servers = 5/' \
+  -e 's/^pm.max_spare_servers = .*/pm.max_spare_servers = 20/' \
+  -e 's/^;pm.max_requests = .*/pm.max_requests = 500/' \
+  /usr/local/etc/php-fpm.d/www.conf
+
+# classifieds
 docker exec classifieds_db mysql -u root -ppassword osclass -e 'source docker-entrypoint-initdb.d/osclass_craigslist.sql'  # Populate DB with content
 
+# shopping
 docker exec shopping /var/www/magento2/bin/magento setup:store-config:set --base-url="http://$PUBLIC_HOSTNAME:$SHOPPING_PORT" # no trailing /
 docker exec shopping mysql -u magentouser -pMyPassword magentodb -e  "UPDATE core_config_data SET value='http://$PUBLIC_HOSTNAME:$SHOPPING_PORT/' WHERE path = 'web/secure/base_url';"
 docker exec shopping /var/www/magento2/bin/magento cache:flush
@@ -23,4 +34,3 @@ docker exec shopping /var/www/magento2/bin/magento indexer:set-mode schedule cat
 docker exec shopping /var/www/magento2/bin/magento indexer:set-mode schedule catalog_product_attribute
 docker exec shopping /var/www/magento2/bin/magento indexer:set-mode schedule catalog_product_price
 docker exec shopping /var/www/magento2/bin/magento indexer:set-mode schedule cataloginventory_stock
-
--- webarena/05_docker_patch_containers.sh
+++ webarena/05_docker_patch_containers.sh
@@ -5,6 +5,16 @@ set -e
 
 source 00_vars.sh
 
+# reddit - make server more responsive
+docker exec forum sed -i \
+  -e 's/^pm.max_children = .*/pm.max_children = 32/' \
+  -e 's/^pm.start_servers = .*/pm.start_servers = 10/' \
+  -e 's/^pm.min_spare_servers = .*/pm.min_spare_servers = 5/' \
+  -e 's/^pm.max_spare_servers = .*/pm.max_spare_servers = 20/' \
+  -e 's/^;pm.max_requests = .*/pm.max_requests = 500/' \
+  /usr/local/etc/php-fpm.d/www.conf
+
+# shopping + shopping admin
 docker exec shopping /var/www/magento2/bin/magento setup:store-config:set --base-url="http://$PUBLIC_HOSTNAME:$SHOPPING_PORT" # no trailing /
 docker exec shopping mysql -u magentouser -pMyPassword magentodb -e  "UPDATE core_config_data SET value='http://$PUBLIC_HOSTNAME:$SHOPPING_PORT/' WHERE path = 'web/secure/base_url';"
 # remove the requirement to reset password
@@ -16,8 +26,9 @@ docker exec shopping_admin /var/www/magento2/bin/magento setup:store-config:set 
 docker exec shopping_admin mysql -u magentouser -pMyPassword magentodb -e  "UPDATE core_config_data SET value='http://$PUBLIC_HOSTNAME:$SHOPPING_ADMIN_PORT/' WHERE path = 'web/secure/base_url';"
 docker exec shopping_admin /var/www/magento2/bin/magento cache:flush
 
+# gitlab
 docker exec gitlab sed -i "s|^external_url.*|external_url 'http://$PUBLIC_HOSTNAME:$GITLAB_PORT'|" /etc/gitlab/gitlab.rb
 docker exec gitlab gitlab-ctl reconfigure
 
+# maps
 docker exec openstreetmap-website-web-1 bin/rails db:migrate RAILS_ENV=development
-
