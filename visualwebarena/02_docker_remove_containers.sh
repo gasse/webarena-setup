d292cfbdac35b6c2fed0f42dc9f6fdc266869f42
Maxime Gasse <maxime.gasse@gmail.com>
2024-12-19 16:00:48 -0500

    ignore docker_remove errors for reset script

--- visualwebarena/02_docker_remove_containers.sh
+++ visualwebarena/02_docker_remove_containers.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# stop if any error occur
-set -e
-
 docker stop classifieds_db classifieds forum shopping wikipedia
 docker remove classifieds_db classifieds forum shopping wikipedia
 
--- webarena/02_docker_remove_containers.sh
+++ webarena/02_docker_remove_containers.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# stop if any error occur
-set -e
-
 docker stop shopping_admin forum gitlab shopping wikipedia openstreetmap-website-db-1 openstreetmap-website-web-1
 docker remove shopping_admin forum gitlab shopping wikipedia openstreetmap-website-db-1 openstreetmap-website-web-1
 
